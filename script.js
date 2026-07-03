6f2ed7804a4120b8147e7c56440ca24aa30f4be2
radhahrithika11299-cloud <radhahrithika11299@gmail.com>
2026-07-03 18:46:54 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,35 @@
-console.log("Hello");
+function openGift() {
+
+    document.getElementById("welcome").style.display = "none";
+
+    document.getElementById("surprise").style.display = "block";
+    
+    document.getElementById("bgMusic").play();
+
+    createHearts();
+
+}
+
+function createHearts() {
+
+    setInterval(function () {
+
+        let heart = document.createElement("div");
+
+        heart.innerHTML = "❤️";
+
+        heart.className = "heart";
+
+        heart.style.left = Math.random() * window.innerWidth + "px";
+
+        heart.style.fontSize = (20 + Math.random() * 30) + "px";
+
+        document.body.appendChild(heart);
+
+        setTimeout(function () {
+            heart.remove();
+        }, 6000);
+
+    }, 300);
+
+}
